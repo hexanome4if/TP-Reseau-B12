8cc97d111396fd083e6159b1f509739b4a76404b
Polo <pablo_mo@orange.fr>
2020-10-14 10:39:02 +0200

    ADD http server

---
+++ src/httpserver/WebServer.java
@@ -0,0 +1,68 @@
+package httpserver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class WebServer {
+
+  protected void start() {
+    ServerSocket s;
+
+    System.out.println("Webserver starting up on port 3000");
+    System.out.println("(press ctrl-c to exit)");
+    try {
+      // create the main server socket
+      s = new ServerSocket(3000);
+    } catch (Exception e) {
+      System.out.println("Error: " + e);
+      return;
+    }
+
+    System.out.println("Waiting for connection");
+    for (; ; ) {
+      try {
+        // wait for a connection
+        Socket remote = s.accept();
+        // remote is now the connected socket
+        System.out.println("Connection, sending data.");
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                remote.getInputStream()));
+        PrintWriter out = new PrintWriter(remote.getOutputStream());
+
+        // read the data sent. We basically ignore it,
+        // stop reading once a blank line is hit. This
+        // blank line signals the end of the client HTTP
+        // headers.
+        String str = ".";
+        while (str != null && !str.equals("")) {
+          str = in.readLine();
+          System.out.println(str);
+        }
+
+        System.out.println("Data:");
+        System.out.println(str);
+        // Send the response
+        // Send the headers
+        out.println("HTTP/1.0 200 OK");
+        out.println("Content-Type: text/html");
+        out.println("Server: Bot");
+        // this blank line signals the end of the headers
+        out.println("");
+        // Send the HTML page
+        out.println("<H1>Welcome to the Ultra Mini-WebServer</H2>");
+        out.flush();
+        remote.close();
+      } catch (Exception e) {
+        System.out.println("Error: " + e);
+      }
+    }
+  }
+
+  public static void main(String args[]) {
+    WebServer ws = new WebServer();
+    ws.start();
+  }
+}
