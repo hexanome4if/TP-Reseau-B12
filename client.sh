1462dc017b15180b5cc02b73a8b29f44cedd0e77
Fabien Narboux <narbouxfabien42@hotmail.fr>
2020-10-21 08:18:00 +0200

    final version, maj *.sh

--- client.sh
+++ client.sh
@@ -1,2 +1,2 @@
-javac -d classes src/*/*/*.java src/*/*/*/*.java src/*/*/*/*/*.java
+javac -d classes src/stream/*/*.java src/stream/*/*/*.java src/stream/*/*/*/*.java
 java -classpath classes stream.client.MainClient localhost 9000
